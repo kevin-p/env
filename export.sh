b51410c5e9d5e1a9c8411202a3abc351d053a3c8
Kevin Personnic <personnic.kevin@gmail.com>
2015-04-05 13:43:57 +0200

    initial version of export.sh

--- export.sh
+++ export.sh
@@ -1,4 +1,20 @@
 #! /usr/bin/zsh
 
-cp -u .* ~/
+#
+#   authors : Kevin Personnic
+#   email   : personnic.kevin@gmail.com
+#   file    : import.sh
+#   desc    : import configuration files from current work environement
+#
+
+#files first
+cp -u .bashrc ~/
+cp -u .dircolors ~/
+cp -u .gitconfig ~/
+cp -u .tmux.conf ~/
+cp -u .vimrc ~/
+cp -u .zshrc ~/
+
+#folders then
+cp -u -r sublime-text-3 ~/.config/
 cp -u -r .fonts ~/
